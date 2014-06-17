8a564953d3e3d0e1988f1dd88d7666b3435952c6
Clarke Harris <clarkeharris@gmail.com>
2014-06-17 01:11:14 -0400

    Added upload and save button which saves to Parse

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,3 @@
-
 // Parse Initialize
 
 Parse.initialize("jGM3Nq64tYbvhiJXvBiTeIaVfxL4MVqGK1n3wPXV", "mQkpxU7BXb28EbhzD7CbZluT4Xxn73hMYeyDye0g");
@@ -7,31 +6,64 @@ Parse.initialize("jGM3Nq64tYbvhiJXvBiTeIaVfxL4MVqGK1n3wPXV", "mQkpxU7BXb28EbhzD7
 
 // Parse Model/Object
 
-var PhotoApp = Parse.Object.extend({
-	className: "Photo"
+var Post = Parse.Object.extend({
+	className: "post"
 });
 
-var photoApp = new PhotoApp();
+var post = new Post();
 
 
 // Parse Collection
 
-var PhotoCollection = Parse.Collection.extend({
-	model: PhotoApp
+var PostCollection = Parse.Collection.extend({
+	model: Post
 });
-
-var photos = new PhotoCollection();
-
 
 // Parse View
 
 var PhotoView = Parse.View.extend({
 
-	template: _.template($('.photo-gram').text()),
+	template: _.template($('.photo-gram-template').text()),
 
 	events: {
-		'click .upload-button' : 'upload-button'
+		'click .upload-button' : 'uploadPhoto'
 	},
 
-	
-})
+	initialize: function(){
+ 	this.listenTo(this.model, 'change', this.render);
+ 
+	 $('.photo-gram').prepend(this.el);
+	 this.render();
+  },
+
+    render: function(){
+    var renderedTemplate = this.template(this.model.attributes)
+    this.$el.html(renderedTemplate);
+  },
+
+// Upload and Save to Parse Button
+
+$('.upload-button').click(function() {
+
+  var fileUploadControl = $(".photo-upload")[0];
+	if (fileUploadControl.files.length > 0) {
+  	var file = fileUploadControl.files[0];
+  	var name = "photo.jpg";
+
+			var parseFile = new Parse.File(name, file);
+			parseFile.save().then(function() {
+			
+				var post = new Post();
+				post.set({
+					"photo"   : parseFile.url(),
+					"caption" : $('photo-caption').val()
+				});
+
+				post.save().then(function(){
+					console.log('Go to bed!')
+				});
+
+		})
+	}
+});
+    
